cf57633b360b6961f03bd3aa6ce3a566fa6718ff
B SHREYANSH <bachchushreyansh@gmail.com>
2024-10-11 14:20:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -235,5 +235,5 @@ app.get('/', (req, res) => {
 
 // Start the Server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
